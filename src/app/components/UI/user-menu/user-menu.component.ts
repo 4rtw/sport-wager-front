c0e5dfd4e351d6263cc353371042b8e5366af54e
Ando Lalaina <59050819+4rtw@users.noreply.github.com>
2021-06-15 08:36:43 +0300

    no changes - rearrange code

--- src/app/components/UI/user-menu/user-menu.component.ts
+++ src/app/components/UI/user-menu/user-menu.component.ts
@@ -1,43 +1,39 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {JwtService} from 'src/app/shared/services/jwt.service';
-import {User} from '../../../shared/model/user.model';
-import {MenuItem} from 'primeng/api';
-import {UserService} from '../../../shared/services/user.service';
-import {Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtService } from 'src/app/shared/services/jwt.service';
+import { User } from '../../../shared/model/user.model';
+import { MenuItem } from 'primeng/api';
+import { UserService } from '../../../shared/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
-    selector: 'app-user-menu',
-    templateUrl: './user-menu.component.html',
-    styleUrls: ['./user-menu.component.css']
+  selector: 'app-user-menu',
+  templateUrl: './user-menu.component.html',
+  styleUrls: ['./user-menu.component.css'],
 })
 export class UserMenuComponent implements OnInit, OnDestroy {
+  user = new User();
+  hasImage = true;
+  items: MenuItem[];
+  userSub: Subscription;
 
-    user = new User();
-    hasImage = true;
-    items: MenuItem[];
-    userSub: Subscription;
+  defaultImage;
 
-    defaultImage;
+  constructor(
+    private router: Router,
+    private jwtService: JwtService,
+    private userService: UserService
+  ) {}
 
-    constructor(
-        private router: Router,
-        private jwtService: JwtService,
-        private userService: UserService,
-    ) {
-    }
+  ngOnInit(): void {
+    this.userSub = this.userService.getUserLoggedIn().subscribe((data) => {
+      if (data instanceof User) {
+        this.user = data;
+      }
+    });
+  }
 
-    ngOnInit(): void {
-        this.userSub = this.userService.getUserLoggedIn().subscribe(
-            data => {
-                if (data instanceof User) {
-                    this.user = data;
-                }
-            }
-        );
-    }
-
-    ngOnDestroy(): void {
-        this.userSub.unsubscribe();
-    }
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
 }
